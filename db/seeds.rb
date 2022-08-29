fc9ba41b892456bebc5fd68413f10e650cf33cd0
Evan Rosson <rossonevan7281@gmail.com>
2022-08-29 09:20:27 -0500

    commit tree 205579e3b93dcb0a3ac0950b187ffc8ebfa8a3c5

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Student.create!(first_name: "Bob", last_name: "Ross", grade: 5)
